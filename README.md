86614202a07e90178f52afdba8077859da52bcfb
Bryant Mairs <susurrus@gmail.com>
2014-10-07 15:44:37 -0700

    Various improvements and cleanups. Red LED now works. Code is organized a little better. Some dead code removed.

--- README.md
+++ README.md
@@ -3,21 +3,21 @@
 This is the code and important files for a data logger currently being created at the UCSC Autonomous Systems Lab.
 
 ## Using the Slogger ##
-Put a valid "config.txt" (see below) file onto the target micro SD card. Insert the card and connect the Slogger to a serial input. A new file will be created on the card with a name like "0001.txt". (The number increases by one for every reset.) When logging is finished, use the "dataExtract.py" tool to remove headers and footers from the data.
+Put a valid "config.txt" (see below) file onto the target micro SD card. Insert the card and connect the Slogger to a serial input. A new file will be created on the card with a name like "0001.txt" (The number increases by one for every reset). When logging is finished, use the "dataExtract.py" tool to remove headers and footers from the data.
 
 ### Other Usage Notes ###
 
-* Put a valid "config.txt" file on the target SD card. See documentation for an example file. NOTE: The Slogger has only been tested with 115200 baud UART.
-
-* Inserting an SD card might cause the device to reset.
+* Create a "config.txt" file on the target SD card with the desired configuration options. See the "Config file" section.
 
 * Resetting the device will cause a new file to be created.
 
-* Data is recorded in chunks of 506 bytes. It is possible for the last 505 bytes of data to be lost.
+* Inserting an SD card might cause the device to reset. This happens when the power supply can't supply power fast enough for the SD card, which consumes a lot of power on initial power-up. This won't be a problem for the device, but will create a new file as the device was reset. To solve this issue, put at least a 200uF electrolytic capacitor across the power lines near the SD card slot.
 
-* The red LED is used to indicate the state of the device. When the red LED is off, the Slogger is able to log data. When the red LED is on, the device has encountered a fatal error and needs to be reset to continue logging.
+* Data is recorded in chunks of 506 bytes.
 
-* **IMPORTANT:** This project is still in development. Chunks of data might be lost. Please take note of any issues, and send them to the developer: Jesse Harkin (jdharkin@ucsc.edu).
+* The yellow LED indicates when the processor has initialized properly and is ready to use.
+
+* The red LED is used to indicate the status of the SD card. When the red LED is off, the Slogger is able to log data. When the red LED is on, the device has encountered a fatal error and needs to be reset to continue logging. This normally indicates a problem with the SD card or the configuration file.
 
 ## Motivation ##
 This project was created because our lab found no available data-loggers that are:
@@ -25,7 +25,7 @@ This project was created because our lab found no available data-loggers that ar
 * Robust
 
 More specifically, the Slogger was designed to meet these requirements:
-* Support continuous 115200 buad UART
+* Support continuous 115200 buad UART recording
 * Maintain the file system while minimizing writes
   * Allocate a file "sweet spot" in size and then increase in that size as needed
 * Integrity checking
@@ -33,12 +33,12 @@ More specifically, the Slogger was designed to meet these requirements:
 * Robust to power/card errors
   * Ensure file system integrity even when card errors out at most inopportune moments. (i.e., a cluster update)
 
-It has also been designed with the posibility for supporting multiple inputs and multiple input types in the future.
+It has also been designed with the possibility for supporting additional inputs in the future.
 
 ## Code ##
 ### Overview ###
 * **Initialization** - The DEE and peripherals are initialized. The config file is read, and a new log file is created on the SD card.
-* **DMA** - The DMA peripheral receives incoming serial data from the UART peripheral, puts it into two ping-pong buffers in memory, then triggers an iterrupt when a buffer is full.
+* **DMA** - The DMA peripheral receives incoming serial data from the UART peripheral, puts it into two ping-pong buffers in memory, then triggers an interrupt when a buffer is full.
 * **Software** - Inside the DMA interrupt, the full ping-pong buffer is copied into the circular buffer.
 Inside the main loop, a chunk of data is taken from the circular buffer, formatted with a header and footer, then sent via SPI to the SD card.
 ![](/docs/images/slogger_data_flow.png "Slogger Data Flow")
@@ -52,7 +52,10 @@ Inside the main loop, a chunk of data is taken from the circular buffer, formatt
 * **DEE** - This library emulates an EEPROM peripheral. This is used inside the NewSDWrite library for naming the new file created on reset.
 * **SD-SPI** and **FSIO** - These libraries are used by NewSDWrite.
 
-* The Uart1 and Timer2 libraries are not used in the current version.
+### Building ###
+Building requires the MPLAB X IDE and XC16 compiler, all available for free on all platforms directly from Microchip's website.
+
+Building entails creating a new MPLAB X project including all *.s and *.c in the root directory and under /Libs. Additionally, some include directories must be specified: the root directory, /Libs, and /Libs/Microchip/Include. Finally, under project properties->xc16-gcc, set the compiler to use the large data model.
 
 ## Data Header/Footer ##
 Data is stored in 512 byte sectors on the SD card, which includes 6 bytes for the header/footer. (Because of this, data in this device is dealt with in chunks of 506 bytes.)
@@ -64,9 +67,7 @@ The checksum is an XOR of all bytes in the data; the header and footer are not i
 ## Config File ##
 The config file should be located on the target SD card and named "config.txt". Configurations are set with a keyword and an integer value for the desired setting. At the moment, the config file sets only one thing: the UART input baud.
 
-NOTE: The Slogger has only been tested at 115200 baud.
-
-**Example:** 
+**Example:**
 ```
 BAUD 115200
 
--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   main.c
  * Author: Jesse
  *
@@ -8,18 +8,20 @@
 #include "CircularBuffer.h"
 #include <xc.h>
 #include <pps.h>
-#include "NewSDWrite.h"
+#include "NewSDWrite/NewSDWrite.h"
 // The UartX.h files need to be included before stddef.h
 #include "Uart2.h"
 #include <stddef.h>
-#include "Libs/Microchip/Include/MDD File System/FSIO.h"
+#include "MDD File System/FSIO.h"
 #include "Node.h"
-#include "Timer2.h"
-#include "DEE Emulation 16-bit.h"
+#include "DEE/DEE Emulation 16-bit.h"
 #include <stdio.h>
 
+// The sector size of the SD card in bytes
 #define SD_SECTOR_SIZE (BYTES_PER_SECTOR)
+// The circular buffer size in bytes, enough to hold 20 full UART2 buffers
 #define CB_SIZE (UART2_BUFFER_SIZE * 20)
+// The state of the SD card, 0 means that no SD card is inserted.
 #define SD_IN (!SD_CD)
 
 /*
@@ -30,77 +32,102 @@
 _FSS(RSS_NO_RAM & SSS_NO_FLASH & SWRP_WRPROTECT_OFF)
 #endif
 _FOSCSEL(FNOSC_FRC & PWMLOCK_OFF);
-_FOSC(FCKSM_CSECMD & OSCIOFNC_OFF & POSCMD_XT);
+_FOSC(FCKSM_CSECMD & OSCIOFNC_ON & POSCMD_NONE);
 _FWDT(FWDTEN_OFF);
 _FICD(JTAGEN_OFF & ICS_PGD2);
 
-void Uart2InterruptRoutine(unsigned char *Buffer, int BufferSize);
-void Timer2InterruptRoutine(void);
-void setLeds(char input);
-void initPins(void);
+static void Uart2InterruptRoutine(unsigned char *Buffer, int BufferSize);
+static void InitPins(void);
 
-CircularBuffer circBuf;
-__eds__ unsigned char __attribute__((eds, space(eds))) cbData[CB_SIZE];
-Sector tempSector;
-int sdConnected;
+static CircularBuffer circBuf;
+static __eds__ unsigned char __attribute__((eds, space(eds))) cbData[CB_SIZE];
+static Sector tempSector;
+// Whether the SD card is connected or not. This is different from the card select
+// pin as it is tracking the "true" state of the SD card, if it's connected and
+// initialized properly.
+static bool sdConnected;
 
 // Debug variables
-uint16_t maxCbGlobal;
-uint16_t writeFailes;
-uint16_t cbSize;
-int cbFilling;
-char string[15];
+static uint16_t maxCbGlobal;
+static uint16_t cbSize;
+static bool cbFilling;
+static char string[15];
 
 int main()
 {
-    //Clock init  M=43, N1,2 = 2 == 39.61MIPS
-    PLLFBD = 43;
-    CLKDIVbits.PLLPOST = 0; // N1 = 2
-    CLKDIVbits.PLLPRE = 0; // N2 = 2
-    OSCTUN = 0;
-    RCONbits.SWDTEN = 0;
+    // Clock init: M=43, N1,2 = 2 == 39.61MIPS
+    {
+        PLLFBD = 43;
+        CLKDIVbits.PLLPOST = 0; // N1 = 2
+        CLKDIVbits.PLLPRE = 0; // N2 = 2
+        OSCTUN = 0;
+        RCONbits.SWDTEN = 0;
 
-    __builtin_write_OSCCONH(0x01); // Initiate Clock Switch to Primary (3?)
+        __builtin_write_OSCCONH(0x01); // Initiate Clock Switch to Primary (3?)
 
-    __builtin_write_OSCCONL(0x01); // Start clock switching
+        __builtin_write_OSCCONL(0x01); // Start clock switching
 
-    while (OSCCONbits.COSC != 0b001); // Wait for Clock switch to occur
+        while (OSCCONbits.COSC != 0b001); // Wait for Clock switch to occur
 
-    while (OSCCONbits.LOCK != 1) {
-    };
-    //End of clock init.
+        while (OSCCONbits.LOCK != 1);
+    }
 
-    if (DataEEInit()) { // must be called prior to any other operation
-        // DataEEInit failed
+    // Initialize EEPROM emulation library.
+    // (must be called prior to any other operation)
+    if (DataEEInit()) {
         FATAL_ERROR();
     }
 
-    initPins();
+    // Set up the peripheral pin mappings
+    InitPins();
 
-    while (!SD_IN);
-    sdConnected = 1;
-
-    // turn on amber LED
-    TRISAbits.TRISA4 = 0;
-    LATAbits.LATA4 = 1;
-
-    Uart2Init(NewSDInit(), Uart2InterruptRoutine);
-    Uart2PrintChar('S');
-
+    // Initialize the CircularBuffer for receiving data
     if (!CB_Init(&circBuf, cbData, CB_SIZE)) {
         FATAL_ERROR();
     }
 
+    // Set sdConnected to the opposite of the card detection signal. This allows
+    // the normal connecting/disconnecting handlers to fire as appropriate without
+    // duplication of code
+    sdConnected = !SD_IN;
+
+    // Turn on the amber LED, indicating that bootup succeeded
+    _LATA4 = 1;
+
+    // Also make sure the red LED is off, we'll use it for error status later
+    _LATA3 = 0;
+
+    // Main event loop
     while (1) {
-        if (SD_IN) {
-            // if the card was just plugged in try to reinitialize
+        // If a card exists...
+        if (MDD_SDSPI_MediaDetect()) {
+            // If the card was just plugged in, try to reinitialize.
             if (!sdConnected) {
-                MEDIA_INFORMATION * Minfo;
-                do {
-                    Minfo = MDD_MediaInitialize();
-                } while (Minfo->errorCode == MEDIA_CANNOT_INITIALIZE);
-                sdConnected = 1;
+                // Initialize the file system
+                if (!FSInit()) {
+                    FATAL_ERROR();
+                }
+
+                // Attempt to read baud rate configuration data from the SD card
+                unsigned long baudRate = NewSDInit();
+                if (!baudRate) {
+                    FATAL_ERROR();
+                }
+
+                // And configure the baud rate accordingly
+                Uart2Init(baudRate, Uart2InterruptRoutine);
+
+                // Attempt to initialize the SD card
+                MEDIA_INFORMATION *minfo = MDD_MediaInitialize();
+                if (minfo->errorCode == MEDIA_CANNOT_INITIALIZE) {
+                    FATAL_ERROR();
+                }
+
+                // Update status, including turning off the red LED
+                sdConnected = true;
+                LATAbits.LATA3 = 0;
             }
+
             // When we are connected and initialized, poll the buffer, if there
             // is data, write it.
             if (CB_PeekMany(&circBuf, tempSector.sectorFormat.data, UART2_BUFFER_SIZE)) {
@@ -114,57 +141,52 @@ int main()
                         sprintf(string, "%d ", cbSize);
                         Uart2PrintStr(string);
                     }
-                    cbFilling = 0;
+                    cbFilling = false;
                     cbSize -= 1;
                 } else { // write failed
                     Uart2PrintStr("WF ");
                 }
             }
-        } else {
-            sdConnected = 0;
+        }
+        // Otherwise, if the card has become disconnected, show the red error LED
+        else if (sdConnected) {
+            LATAbits.LATA3 = 1;
+            sdConnected = false;
         }
     }
 }
 
-// calculates a basic byte Xor checksum of some data
-
-char checksum(char * data, int dataSize)
-{
-    char sum = 0;
-    int i;
-    for (i = 0; i < dataSize; i++) {
-        sum ^= data[i];
-    }
-    return sum;
-}
-
 /**
- * Initialize pin mappings and set pins as digital.
+ * Initialize input/output pins for the processor.
  */
-void initPins(void)
+static void InitPins(void)
 {
     // And configure the Peripheral Pin Select pins:
     PPSUnLock;
     // To enable ECAN1 pins: TX on 7, RX on 4
     PPSOutput(OUT_FN_PPS_C1TX, OUT_PIN_PPS_RP39);
-    PPSInput(PPS_C1RX, PPS_RP20);
+    PPSInput(IN_FN_PPS_C1RX, IN_PIN_PPS_RP20);
 
     // To enable UART2 pins: TX on 11, RX on 13
     PPSOutput(OUT_FN_PPS_U2TX, OUT_PIN_PPS_RP43);
-    PPSInput(PPS_U2RX, PPS_RPI45);
+    PPSInput(IN_FN_PPS_U2RX, IN_PIN_PPS_RPI45);
 
     // enable the SPI stuff: clock, in, out
     PPSOutput(OUT_FN_PPS_SCK2, OUT_PIN_PPS_RP41);
     PPSOutput(OUT_FN_PPS_SDO2, OUT_PIN_PPS_RP40);
-    PPSInput(PPS_SDI2, PPS_RP42);
+    PPSInput(IN_FN_PPS_SDI2, IN_PIN_PPS_RP42);
     PPSLock;
 
-    // Disable A/D functions on pins
+    // And enable both the LED pins as outputs
+    _TRISA3 = 0; // Red LED
+    _TRISA4 = 0; // Amber LED
+
+    // Disable A/D functions on all pins
     ANSELA = 0;
     ANSELB = 0;
 }
 
-void Uart2InterruptRoutine(unsigned char *Buffer, int BufferSize)
+static void Uart2InterruptRoutine(unsigned char *Buffer, int BufferSize)
 {
     CB_WriteMany(&circBuf, Buffer, BufferSize, true); // fail early
 
@@ -173,5 +195,5 @@ void Uart2InterruptRoutine(unsigned char *Buffer, int BufferSize)
     if (cbSize > maxCbGlobal) {
         maxCbGlobal = cbSize;
     }
-    cbFilling = 1;
+    cbFilling = true;
 }
