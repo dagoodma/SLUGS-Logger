9fa5a85e83acf668ec147b4c19c2d1b412345ea7
Bryant <bwmairs@ucsc.edu>
2014-11-17 13:43:39 -0800

    Make extractData.py work on a set of log files.

--- Testing/extractData.py
+++ Testing/extractData.py
@@ -1,7 +1,7 @@
 """
 This script will copy a log file to a new file, removing headers and footers along the way.
-Run in command line: "python3 dataExtract.py [log file] [desination file]"
-Example: "python3 dataExtract.py 0001.txt Log21Aug.txt"
+Run in command line: "python3 dataExtract.py LOG_FILE [LOG_FILE2 ...]"
+Example: "python3 dataExtract.py 0001.log"
 """
 
 import sys
@@ -14,8 +14,8 @@ def checkSum(data):
     return cs
 
 # Process input arguments
-if len(sys.argv) < 3 :
-    print("This script requires 2 arguments: a logger file to extract from and a file to dump extracted data. WARNING: The second file will be overwritten!")
+if len(sys.argv) < 2 :
+    print("This script requires at least 1 argument: a logger file to extract from.")
     exit()
 
 # Set some constants describing the file format
@@ -27,34 +27,42 @@ FILE_NUMBER_POS = 2
 CHECKSUM_POS = -3
 
 # Open files
-with open(sys.argv[1], "rb") as slog, open(sys.argv[2], "wb") as outlog:
+for f in sys.argv[1:]:
+    f_out = f.replace('.log', '_extracted.log')
+    print("Decoding {} into {}.".format(f, f_out))
+    with open(f, "rb") as slog, open(f_out, "wb") as outlog:
 
-    # Main loop, reading file by sectors and processing each of them.
-    nextSector = slog.read(512)
-    fileNumber = nextSector[FILE_NUMBER_POS]
-    sectorNumber = 0
-    while nextSector:
-        # Verify header & footer tags
-        if nextSector[:HEADER_TAG_LEN] != b'%^':
-            print("ERROR: Header tag failed at sector {}, exiting.".format(sectorNumber))
-            sys.exit()
-        if nextSector[-FOOTER_TAG_LEN:] != b'%$':
-            print("ERROR: Footer tag failed at sector {}, exiting.".format(sectorNumber))
-            sys.exit()
-
-        # Verify header number
-        if nextSector[FILE_NUMBER_POS] != fileNumber:
-            print("WARNING: File number mismatch at sector {}. This usually happens when files don't end on a 128kiB boundary. Please verify that all data is intact if possible.".format(sectorNumber))
-            sys.exit()
-
-        # Verify footer checksum
-        if nextSector[CHECKSUM_POS] != checkSum(nextSector[HEADER_LEN:-FOOTER_LEN]):
-            print("ERROR: Checksum failed at sector {}, exiting.".format(sectorNumber))
-            sys.exit()
-
-        # Output the actual sector data and prepare for next iteration
-        outlog.write(nextSector[HEADER_LEN:-FOOTER_LEN])
-        sectorNumber += 1
+        # Main loop, reading file by sectors and processing each of them.
         nextSector = slog.read(512)
+        fileNumber = nextSector[FILE_NUMBER_POS]
+        sectorNumber = 0
+        error = False
+        while nextSector:
+            # Verify header & footer tags (Errors here may signify dataloss)
+            if nextSector[:HEADER_TAG_LEN] != b'%^':
+                print("ERROR: Header tag failed at sector {}.".format(sectorNumber))
+                error = True
+                break
+            if nextSector[-FOOTER_TAG_LEN:] != b'%$':
+                print("ERROR: Footer tag failed at sector {}.".format(sectorNumber))
+                error = True
+                break
 
-    print("Successfully decoded {}, comprising {} bytes.".format(sys.argv[2], (sectorNumber + 1) * 506))
+            # Verify header number (Errors here are generally not indicative of data loss)
+            if nextSector[FILE_NUMBER_POS] != fileNumber:
+                print("WARNING: File number mismatch at sector {}. This is likely not an error, as it usually happens when files don't end on a 128kiB boundary. Please verify that all data is intact if possible.".format(sectorNumber))
+                error = True
+                break
+
+            # Verify footer checksum (Errors here may signify dataloss)
+            if nextSector[CHECKSUM_POS] != checkSum(nextSector[HEADER_LEN:-FOOTER_LEN]):
+                print("ERROR: Checksum failed at sector {}.".format(sectorNumber))
+                error = True
+                break
+
+            # Output the actual sector data and prepare for next iteration
+            outlog.write(nextSector[HEADER_LEN:-FOOTER_LEN])
+            sectorNumber += 1
+            nextSector = slog.read(512)
+
+        print("Decoded {} bytes into '{}'.".format((sectorNumber + 1) * 506, f_out))
